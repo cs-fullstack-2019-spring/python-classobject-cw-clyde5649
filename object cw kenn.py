e871107c37cec031f2f3044d468891618127ba89
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 18:07:25 -0600

    workk

---
+++ object cw kenn.py
@@ -0,0 +1,99 @@
+    def main():
+        # problem1()
+        # problem2()
+        # problem3()
+        # problem4()
+
+    def  problem1():
+
+
+        class Dog:
+
+    def  __init__(self, breed="", color="", gender=""):
+           self.breed = breed
+           self.color = color
+           self.gender = gender
+
+
+    def printAll(self):
+        print(self.breed, self.color, self.gender)
+
+    def NewDog = Dog("breed", "color" , "gender")
+                    NewDog.printAll()
+
+
+
+
+
+    def problem2():
+
+        USerTalk =""
+
+           while(USerTalk != "="):
+            USerTalk = input("quit")
+
+            if(USerTalk = "something else"):
+                print("enter other string")f(USerTalk = "something else"):
+                print("enter other string")
+
+
+
+
+
+
+
+
+
+    def problem3():
+
+      class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def problem3():
+
+      class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      if __name__ == '__main__':
+        main()
